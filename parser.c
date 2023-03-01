fb4c6033b61739c52b892fa61e324ec073c96fb2
Cmois <cmois@student.42.fr>
2023-03-01 13:48:22 +0100

    making it bootifol

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: cmois <cmois@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 13:29:47 by cmois             #+#    #+#             */
-/*   Updated: 2023/03/01 13:39:32 by cmois            ###   ########.fr       */
+/*   Updated: 2023/03/01 13:47:55 by cmois            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,36 @@ int isItConvertible(char c)
         return (1);
     return (0);
 }
+
+bool    isItWhatWeWant(t_conversion *what, const char *str, int i)
+{
+    if ((str[i] == ' ' && (str[i + 1] >= '0' && str[i + 1] <= '9')) || (str[i] == '0' && (str[i + 1] >= '0' && str[i + 1] <= '9'))
+                || (str[i] == '+' && (str[i + 1] >= '0' && str[i + 1] <= '9')) || (str[i] == '-' && (str[i + 1] >= '0' && str[i + 1] <= '9'))
+                || (str[i] == '#' && (str[i + 1] >= '0' && str[i + 1] <= '9')) || str[i] == '#')
+    {
+        if (str[i] == '#')
+             what->_hashtag = true;
+        if (str[i] == '-')
+            what->_isMinus = true;
+        if (str[i] == '+')
+             what->_isPlus = true;                                                                           //ERROR CHECK FOR ARGS
+        if (str[i] == ' ')
+            what->_isSpace = true;
+        if (str[i] == '0')
+             what->_isZero = true;
+        return (true);
+    }
+    return (false);
+}
+
+// void    getNumbers(const char *str, int i, t_conversion *what)
+// {
+//     while (str[i] >= '0' && str[i] <= '9')
+//     {
+//         what->_ARGVAL = joinNumbers(what, str[i]);
+//         i++;
+//     }
+// }
 
 
 void    parser(t_conversion *what, const char *str)
@@ -37,21 +67,10 @@ void    parser(t_conversion *what, const char *str)
                 doTheJob(what, str[i]);
                 i++;
             }
-            if ((str[i] == ' ' && (str[i + 1] >= '0' && str[i + 1] <= '9')) || (str[i] == '0' && (str[i + 1] >= '0' && str[i + 1] <= '9'))
-                || (str[i] == '+' && (str[i + 1] >= '0' && str[i + 1] <= '9')) || (str[i] == '-' && (str[i + 1] >= '0' && str[i + 1] <= '9'))
-                || (str[i] == '#' && (str[i + 1] >= '0' && str[i + 1] <= '9')) || str[i] == '#')
+            if (isItWhatWeWant(what, str, i))
             {
-                if (str[i] == '#')
-                    what->_hashtag = true;
-                if (str[i] == '-')
-                    what->_isMinus = true;
-                if (str[i] == '+')
-                    what->_isPlus = true;                                                                           //ERROR CHECK FOR ARGS
-                if (str[i] == ' ')
-                    what->_isSpace = true;
-                if (str[i] == '0')
-                    what->_isZero = true;
                 i++;
+                // getNumbers(str, i, what);
                 while (str[i] >= '0' && str[i] <= '9')
                 {
                     what->_ARGVAL = joinNumbers(what, str[i]);
